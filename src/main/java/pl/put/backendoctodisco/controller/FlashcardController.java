c47cd420d98d44e543028e31740b42861eb66d7e
jkolodziejski <jakub.m.kolodziejski@student.put.poznan.pl>
2022-11-07 18:16:27 +0100

    Initial commit

--- src/main/java/pl/put/backendoctodisco/controller/FlashcardController.java
+++ src/main/java/pl/put/backendoctodisco/controller/FlashcardController.java
@@ -1,9 +1,11 @@
 package pl.put.backendoctodisco.controller;
 
-import antlr.Token;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +18,8 @@ import pl.put.backendoctodisco.exceptions.TokenNotFoundException;
 import pl.put.backendoctodisco.service.FlashcardService;
 import pl.put.backendoctodisco.service.UserService;
 import pl.put.backendoctodisco.utils.AuthToken;
+import java.util.List;
 
-import static pl.put.backendoctodisco.utils.AuthToken.*;
-import static pl.put.backendoctodisco.utils.AuthToken.State.EMPTY;
 
 @RestController
 @RequestMapping("/flashcard")
@@ -51,5 +52,27 @@ public class FlashcardController {
 
         Flashcard createdFlashcard = flashcardService.createFlashcard(new Flashcard(foundUser, flashcardRequest));
         return new ResponseEntity<>(createdFlashcard, HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Get flashcards from database by page",
+            notes = "Returns list of flashcard")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "The resource has been fetched and transmitted in the message body"),
+            @ApiResponse(code = 403, message = "Token not found or token expired (error specified in the message)")
+    })
+    @GetMapping("/pages")
+    private ResponseEntity<List<Flashcard>> getFlashcards(@RequestHeader(name = HttpHeaders.AUTHORIZATION, defaultValue = "") String authToken,@PageableDefault(value = 25) Pageable pageable) throws TokenNotFoundException, TokenExpiredException {
+        User foundUser = userService.findUserByAuthToken(authToken);
+
+        AuthToken tokenToCheck = new AuthToken(foundUser.getAuthToken());
+
+        switch (tokenToCheck.checkToken()) {
+            case EMPTY -> throw new TokenNotFoundException();
+            case EXPIRED -> throw new TokenExpiredException();
+        }
+
+        List<Flashcard> flashcardList = flashcardService.getAllFlashcards(pageable);
+        return  new ResponseEntity<>(flashcardList,HttpStatus.OK);
     }
 }
--- src/main/java/pl/put/backendoctodisco/service/FlashcardService.java
+++ src/main/java/pl/put/backendoctodisco/service/FlashcardService.java
@@ -1,9 +1,13 @@
 package pl.put.backendoctodisco.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.put.backendoctodisco.entity.Flashcard;
 import pl.put.backendoctodisco.repository.FlashcardRepository;
+
+import java.util.List;
 
 @Service
 public class FlashcardService {
@@ -17,4 +21,10 @@ public class FlashcardService {
     public Flashcard createFlashcard(Flashcard flashcard){
         return repository.save(flashcard);
     }
+
+    public List<Flashcard> getAllFlashcards(Pageable pageable){
+        Page<Flashcard> page =repository.findAll(pageable);
+        return   page.getContent();
+    }
+
 }
