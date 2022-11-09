4c8a862399e983c513d26d26bc2704bb384aa3fc
PiotrKolankiewicz <piotr.kolankiewicz@student.put.poznan.pl>
2022-11-10 00:54:05 +0100

    verification finished

--- src/main/java/pl/put/backendoctodisco/exceptions/TokenNotFoundException.java
+++ src/main/java/pl/put/backendoctodisco/exceptions/TokenNotFoundException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import pl.put.backendoctodisco.utils.ApiError;
 
 public class TokenNotFoundException extends Throwable {
-    public ApiError error = new ApiError(HttpStatus.FORBIDDEN, "Token not found in the database");
+    public ApiError error = new ApiError(HttpStatus.FORBIDDEN, "Token not found in the database or written incorrectly in the first place.");
 }
