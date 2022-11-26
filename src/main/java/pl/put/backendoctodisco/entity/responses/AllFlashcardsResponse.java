bcfb36acda96f8acd08c190c50057a06c721f54e
jkolodziejski <jakub.m.kolodziejski@student.put.poznan.pl>
2022-11-26 14:05:52 +0100

    #74 Documentation and handling Exceptions

--- src/main/java/pl/put/backendoctodisco/entity/responses/AllFlashcardsResponse.java
+++ src/main/java/pl/put/backendoctodisco/entity/responses/AllFlashcardsResponse.java
@@ -1,58 +1,49 @@
 package pl.put.backendoctodisco.entity.responses;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import pl.put.backendoctodisco.entity.Flashcard;
 
-import java.io.Serializable;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@ToString
 public class AllFlashcardsResponse  {
 
-    @ApiModelProperty(notes = "List of flashcards", example = """
-            "size": 1,
-                "flashcards": [
-                    {
-                        "id": 1,
-                        "language": "pl",
-                        "word": "Poland",
-                        "isGlobal": false,
-                        "userId": 1,
-                        "alias": [
-                            "test",
-                            "test1"
-                        ]
-                    }
-               ]
-            }""")
+    @ApiModelProperty(notes = "Metadata of Response ")
+    private final Metadata metadata;
+
+    @ApiModelProperty(notes = "List of flashcards with Alias")
     private final List<FlashcardResponse> flashcards;
 
-    @ApiModelProperty(notes = "Size of flashcard list", example = "2")
-    private final long size;
-    private final Integer totalPages;
+
 
     public AllFlashcardsResponse(List<FlashcardResponse> flashcards){
+        this.metadata = new Metadata(flashcards.size(),1);
         this.flashcards = flashcards;
-        this.size = flashcards.size();
-        totalPages = 1;
+
     }
 
     public AllFlashcardsResponse(List<FlashcardResponse> flashcards, Page<Flashcard> page) {
+        this.metadata = new Metadata(page.getNumberOfElements(),page.getTotalPages());
         this.flashcards = flashcards;
-        this.size = page.getTotalElements();
-        this.totalPages = page.getTotalPages();
+
+
 
     }
+
 
     public Map<String, Object>  generateResponse(){
         Map<String, Object> map  = new HashMap<>();
         map.put("flashcards",flashcards);
-        map.put("size",size);
-        map.put("Number of pages", totalPages);
+        map.put("Metadate",metadata);
         return  map;
     }
+
 
 }
