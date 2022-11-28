44f5090aa247954240598a271fe1c6b789bd7514
PiotrKolankiewicz <piotr.kolankiewicz@student.put.poznan.pl>
2022-11-28 19:05:01 +0100

    swagger corrected for quiz

--- src/main/java/pl/put/backendoctodisco/entity/responses/Quiz.java
+++ src/main/java/pl/put/backendoctodisco/entity/responses/Quiz.java
@@ -12,7 +12,56 @@ import java.util.List;
 @ToString
 @Getter
 public class Quiz {
-    @ApiModelProperty(notes = "List of questions", required = true)
+    @ApiModelProperty(notes = "List of questions", example="[\n" +
+            "        {\n" +
+            "            \"answers\": [\n" +
+            "                {\n" +
+            "                    \"word\": \"approach\",\n" +
+            "                    \"translation\": \" podchodzić\"\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"word\": \"carefully\",\n" +
+            "                    \"translation\": \"ostrożnie\"\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"word\": \"chemical\",\n" +
+            "                    \"translation\": \"chemikalia\"\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"word\": \"create\",\n" +
+            "                    \"translation\": \" tworzyć\"\n" +
+            "                },\n" +
+            "                {\n" +
+            "                    \"word\": \"evil\",\n" +
+            "                    \"translation\": \" nikczemny\"\n" +
+            "                }\n" +
+            "            ]\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"answer\": {\n" +
+            "                \"word\": \"experiment\",\n" +
+            "                \"translation\": \"eksperyment\"\n" +
+            "            },\n" +
+            "            \"false_options\": [\n" +
+            "                \" starannie\",\n" +
+            "                \"zły\",\n" +
+            "                \" podchodzić\"\n" +
+            "            ]\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"flashcard\": {\n" +
+            "                \"id\": 31,\n" +
+            "                \"language\": \"en\",\n" +
+            "                \"word\": \"noise\",\n" +
+            "                \"isGlobal\": true,\n" +
+            "                \"userId\": 36,\n" +
+            "                \"alias\": [\n" +
+            "                    \"hałas\",\n" +
+            "                    \" szum \"\n" +
+            "                ]\n" +
+            "            }\n" +
+            "        }\n" +
+            "]", required = true)
     @Column(name = "questions", nullable = false)
     private List<QuizQuestion> questions;
 
